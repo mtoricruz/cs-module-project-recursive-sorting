427c05dc56ec4db1d7601ce43ad43f09b871f8a0
toricruzmendiola <mtoricruz@gmail.com>
2020-07-21 12:53:21 -0700

    recursive binary search implemented and passing test, moving to merge sort

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,7 +1,18 @@
-# TO-DO: Implement a recursive implementation of binary search
+ # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
-    # Your code here
+    while start <= end:
+        midpoint = start + (end - start) // 2
+        mid_value = arr[midpoint]
 
+        if mid_value == target:
+            return midpoint
+        elif target < mid_value:
+            return binary_search(arr, target, start, midpoint - 1)
+        else:
+            return binary_search(arr, target, midpoint + 1, end)
+    return - 1
+
+# Question: I edited my iterative version to pass the recursive test. On my iterative version, i explicitly state the start/end point and store them in start/end index variables, why does that break here in a recursive search? Isn't the start/end index a base case itself?
 
 # STRETCH: implement an order-agnostic binary search
 # This version of binary search should correctly find 
@@ -11,4 +22,4 @@ def binary_search(arr, target, start, end):
 # or iteratively
 def agnostic_binary_search(arr, target):
     # Your code here
-
+    pass
